173e0c1680d7480d1c20907bf2796526eeeccdc1
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-12-29 22:26:41 +0100

    prettier: Prettier related update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,21 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import { theme } from "./src/theme"
+import { theme } from "./src/theme";
 
 export const onClientEntry = () => {
   window.onload = () => {
     document.documentElement.style.setProperty(
       "--background-color",
       theme.colors.background
-    )
+    );
     document.documentElement.style.setProperty(
       "--dark-background-color",
       theme.colors.darkBackground
-    )
+    );
     document.documentElement.style.setProperty(
       "--primary-color",
       theme.colors.primary
-    )
-  }
-}
+    );
+  };
+};
